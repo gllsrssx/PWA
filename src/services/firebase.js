e31f804cd9fa7f9a2771096076e29be48fbb3a60
gilles20393 <gilles1996@outlook.com>
2024-03-07 01:45:40 +0100

    Add react-dev-utils and enable Firestore persistence

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,37 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyAPF1KB8Eq9X9cFCIqYOfINNyljAVPqW64",
+  authDomain: "react-ep.firebaseapp.com",
+  projectId: "react-ep",
+  storageBucket: "react-ep.appspot.com",
+  messagingSenderId: "384745751994",
+  appId: "1:384745751994:web:075f257eb78a4c1c950983",
+  measurementId: "G-37369YJVSN"
 };
+
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-export const firestoreDB = getFirestore(firebaseApp);
-console.log("initialized firebase connection");
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firestore
+const firestoreDB = getFirestore(app);
+
+// Enable offline persistence
+enableIndexedDbPersistence(firestoreDB)
+  .then(() => {
+    console.log("Firestore persistence enabled");
+  })
+  .catch((err) => {
+    if (err.code === 'failed-precondition') {
+      console.log("Multiple tabs open, persistence can only be enabled in one tab at a a time.");
+    } else if (err.code === 'unimplemented') {
+      console.log("The current browser does not support all of the features required to enable persistence");
+    }
+  });
+
+export { firestoreDB, analytics };
